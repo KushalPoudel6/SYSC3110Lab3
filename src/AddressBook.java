d3d91309abf2d4e7c652e12cd49dabc3179a5cc1
Kushal Poudel <kpoud2@gmail.com>
2025-09-15 10:34:03 -0400

    added main

---
+++ src/AddressBook.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> info;
+
+    public AddressBook(){
+        info = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo b){
+        info.add(b);
+    }
+
+    public boolean removeBuddy(BuddyInfo b){
+        return info.remove(b);
+    }
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
